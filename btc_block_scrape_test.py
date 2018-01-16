2c9c6d02c7c786a5a7ab9e6be262bae0300f120b
Joe Dechery <jdechery@gmail.com>
2018-01-15 16:57:02 -0800

    statsmodels and arch exploration

--- btc_block_scrape_test.py
+++ btc_block_scrape_test.py
@@ -1,11 +1,13 @@
+# block chain transaction scrape
 import requests
 import pandas as pd
-# block chain transaction scrape
 
 homeurl = 'https://blockchain.info/rawblock/'
 block_hash_ex = '0000000000000000005048da3ecea6695cb217ebf85f1a03fcf11c006de078c7'
 
 response = requests.get(homeurl+block_hash_ex)
-
-XY = []
-for tx in a['tx']:
+blockdata = response.json()
+prev_hash = blockdata['prev_block']
+txfee = []  # (index, time, numin, numout, fee)
+for tx in blockdata['tx']:
+    temp = (tx[''])
--- forecast_model_explore.py
+++ forecast_model_explore.py
@@ -1,15 +1,52 @@
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
 import pandas as pd
-from Btcblock import load_data
+from Btcblock import etl_utils
 import matplotlib
 import matplotlib.pyplot as plt
+import numpy as np
 
 chart_files = ['transaction-fees', 'mempool-size', 'hash-rate', 'market-price', 'n-transactions', 'blocks-size', 'n-transactions-per-block']
-df = load_data.chartcsv2dataframe(chart_names=chart_files)
-df = df.loc[-1000:, :]
+df = etl_utils.chartcsv2dataframe(chart_names=chart_files)
+df.set_index('time', inplace=True)
+#df = df.iloc[-1000:, :]
+# %%
+#df[chart_files].apply(lambda x: x/np.max(x)).iloc[1400:, :].plot()
+df['transaction-fees'].iloc[-500:].plot()
+# df['mempool-size'].plot()
+plt.show()
+# %% statsmodels arima
+from statsmodels.tsa.arima_model import ARIMA
+model_var = 'transaction-fees'
+forecast_dur = 6
+order = (4, 1, 2)
+# test_data = df['mempool-size'].iloc[1500:-forecast_dur]
+test_data = df[model_var].iloc[1500:-forecast_dur]
+model = ARIMA(test_data, order)
+fitted_model = model.fit(maxiter=100, transparams=True)
+
+# % plot results/residuals
+# forecast_dates = pd.date_range(start='2018-01-11', end='2018-01-19').to_datetime()
+# yhat = fitted_model.predict()
+yfuture = fitted_model.forecast(steps=forecast_dur)#start=len(test_data)-1, end=len(test_data)+10)
+fig, ax = plt.subplots(figsize=(8, 4))
+plt.plot_date(df.index[-forecast_dur:], yfuture[0], fmt='-k')
+plt.plot_date(df.index[-forecast_dur:], yfuture[0]+yfuture[1], fmt=':k')
+plt.plot_date(df.index[-forecast_dur:], yfuture[0]-yfuture[1], fmt=':k')
+# test_data.plot()
+df[model_var].iloc[1500:].plot()
+# %%
+from statsmodels.tsa.stattools import acf, pacf
+dat = np.log(test_data)
+dat = dat - dat.rolling(5).mean()
+dat.fillna(method='bfill', inplace=True)
+lag_acf = acf(dat, nlags=10)
+lag_pacf = pacf(np.log(test_data), nlags=10, method='ols')
+plt.plot(lag_pacf)
 
 # %%
+df['mempool-size'].iloc[1000:].plot()
+# %% sklearn linear regression
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 nlag = 10;
 cutoff = 1400;
 model = LinearRegression()
@@ -30,7 +67,3 @@ target = df.loc[1200:, target_name]
 X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.1)
 model.fit(X_train, y_train)
 print(model.score(X_test, y_test))
-
-# %%
-target.plot()
-plt.show()
